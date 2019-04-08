1941a70a865293af48ef5a41d0a03ffe3043150e
Franco Sanchez <22228100+Femli@users.noreply.github.com>
2019-04-08 14:44:43 -0700

    Update Basic_Template.py

--- Programs/Basic_Template.py
+++ Programs/Basic_Template.py
@@ -1,22 +1,30 @@
 ## THIS IS A BASIC PROGRAM TEMPLATE THAT CLOSES THE PROGRAM WHEN WE PRESS THE TOP LEFT X BUTTON ##
 
-import pygame
+#IMPORTING THE PYGAME MODULE
+import pygame 
 
-pygame.init()
+#THIS ENSURES THAT THE GAME WILL START
+pygame.init() 
 
-screenWidth = 400
-screenHeight = 500
+#HOW MANY PIXELS WIDE AND TALL OUR GAME WINDOW IS
+screenWidth = 400 
+screenHeight = 500 
 
-screen = pygame.display.set_mode((screenWidth, screenHeight))
+#CREATING OUR ACTUAL GAME WINDOW DISPLAY; IT IS CALLED 'screen'
+screen = pygame.display.set_mode((screenWidth, screenHeight)) 
 
-gameOff = False
+#THIS IS NECESSARY FOR WHEN WE WANT TO SHUT OFF OUR INFINITE WHILE LOOP
+gameOff = False 
 
-While not gameOff:
+#THIS WHILE LOOP WILL REPEAT OVER AND OVER, AND MAKE SURE THAT OUR GAME VISUALS ARE CONSTANTLY UPDATING
+While not gameOff: 
   
-  for event in pygame.event.get():
+  #THIS IS NECESSARY TO CAPTURE THE EVENTS THAT ARE HAPPENING IN OUR GAME
+  for event in pygame.event.get(): 
     
-    if event.type() == pygame.QUIT:
-      gameOff = True
-      pygame.quit()
+    #IF THE EVENT IS PRESSING THE 'X' BUTTON IN THE TOP LEFT OF THE SCREEN, THEN CLOSE 
+    if event.type() == pygame.QUIT: 
+      gameOff = True #TURN OFF THE INFINITE WHILE LOOP
+      pygame.quit() #SHUT OFF THE PROGRAM
       
   
